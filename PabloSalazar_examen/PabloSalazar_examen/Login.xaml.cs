08206778a0142b167327679e8298143415a19d90
Rastapablo1 <56987410+Rastapablo1@users.noreply.github.com>
2020-10-31 00:34:27 -0500

    Se crea el repositorio del examen

---
+++ PabloSalazar_examen/PabloSalazar_examen/Login.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PabloSalazar_examen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            string user = txtUser.Text;
+            string pass = txtPass.Text;
+
+            if (string.IsNullOrEmpty(txtUser.Text) || string.IsNullOrEmpty(txtPass.Text))
+                DisplayAlert("Sin Datos", "Ingrese usuario y contraseña", "OK");
+            else
+            {
+                if (txtUser.Text == "estudiante2020" && txtPass.Text == "uisrael2020")
+                {
+                    DisplayAlert("Ingreso existoso", "", "OK");
+                    await Navigation.PushAsync(new Registro(user, pass));
+                }
+                else
+                    DisplayAlert("Ingreso Fallido", "Ingrese correctamente los datos", "OK");
+            }
+        }
+    }
+}
